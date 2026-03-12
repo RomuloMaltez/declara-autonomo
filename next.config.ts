8b6374b0a1ff688ba435b63322f03604bb6c2001
Tatmiki <tatmiki@outlook.com>
2026-03-12 13:56:07 -0300

    Config para URL principal

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    assetPrefix: "/semec/declara-autonomo",
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
